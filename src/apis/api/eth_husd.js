b1cfbb0531276bc6ed5d3f806d23f9787cecdc0e
Qiu-zm <1057390813@qq.com>
2021-05-11 14:54:58 +0800

    update add link.abi

--- src/apis/api/eth_husd.js
+++ src/apis/api/eth_husd.js
@@ -2,7 +2,7 @@ import { pairAbi } from '../../abi/pair.abi.js' // pair-abi
 import { poolAbi } from '../../abi/pool.abi.js' // pool-abi
 
 import { ethAbi } from '../../abi/eth.abi.js' // pre-abi
-import { usdtAbi } from '../../abi/usdt.abi.js' // next-abi
+import { husdAbi } from '../../abi/husd.abi.js' // next-abi
 import Contract from '../contract/index.js'
 import API from '../contract/api.js'
 
@@ -13,15 +13,15 @@ const usdtAddr = `0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047` // next
 const trsUsdtPairAddr = `0x9df6d7534aae306b8f5ea0f9eef52b48f48e52bd` // pre-next-pair
 const trsUsdtPoolAddr = `0xb866928453cdCeEb30fADEe8a9C18007dF05802B` // pre-next-pool
 
-// pre-next  USDT/HUSD
+// pre-next  ETH/HUSD
 const usdtTokenAddr = usdtAddr
 const huiwanTokenAddr = trsAddr
 const huiwanUsdtMdexAddr = trsUsdtPairAddr
 const huiwanUsdtLoopAddr = trsUsdtPoolAddr
 
-// pre-next  USDT/HUSD
+// pre-next  ETH/HUSD
 const huiwanTokenABI = ethAbi //pre-abi
-const usdtTokenABI = usdtAbi //next-abi
+const usdtTokenABI = husdAbi //next-abi
 const huiwanUsdtMdexABI = pairAbi
 const huiwanUsdtLoopABI = poolAbi
 
