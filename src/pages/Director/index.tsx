679c9eded3d39ba04a7799ec6dd7d449a1a36f76
Qiu-zm <1057390813@qq.com>
2021-05-13 16:45:50 +0800

    Change Director button and balance

--- src/pages/Director/index.tsx
+++ src/pages/Director/index.tsx
@@ -1,10 +1,28 @@
 // import React,{ Fragment } from "react";
 import React , { useState }from "react";
 import { useTranslation } from "react-i18next"
+import styled from 'styled-components'
+
 import Data from '../../apis/api/data.js'
+import API from '../../apis/api/six.js'
 
 import './index.css'
 
+const Approvediv = styled.div`
+    padding:10px 20px;
+    background-color:#fff;
+    color:#000;
+    position: fixed;
+    top:30%;
+    left:50%;
+    border-radius:10px;
+    box-shadow: 0px 0px 6px #ccc;
+    font-size:14px;
+    line-height:20px;
+    text-align:center;
+    z-index:2999;
+    transform: translateX(-50%);
+`
 
 const formatNum = function (str: string|number) {
     if(str*1<0) return 0
@@ -26,12 +44,24 @@ const formatNum = function (str: string|number) {
 
     return temp
 }
+let timers
 
 export default function Director() {
     const { t } = useTranslation();
     const [rate, setRate] = useState(0)
     const [allBalance, setAllBalance] = useState(0)
     const [allBlock, setAllBock] = useState(0.00)
+    const [isApprovediv, setApprovediv] = useState(false) // 授权/非授权
+    const [balance, setBalance] = useState(0.00)
+
+    const toast = () => {
+        setApprovediv(true)
+        timers && clearTimeout(timers)
+        timers = setTimeout(() => {
+            setApprovediv(false)
+            clearTimeout(timers)
+        }, 2000);
+    }
 
     //trs价格
     Data.getTrsRate().then(res => {
@@ -46,17 +76,28 @@ export default function Director() {
         // console.log(`setAllBock`,res)
         setAllBock(res)
     })
+    API.getWalletAllTrs().then(res => {
+        // console.log(`setBalance`,res)
+        setBalance(res)
+    })
+    
     // 提取
     function extract() {
-        console.log('extract');
+        // console.log('extract');
+        
+        toast()
     }
     // 锁定
     function lockin() {
-        console.log('lockin');
+        // console.log('lockin');
+        toast()
     }
     return (
         <>
             <div className="driectorList">
+            {isApprovediv && (
+                        <Approvediv>暂未开放</Approvediv>
+                    )}
                 <div className="driectorItem">
                     <div className="driectorTitle">
                         <img src={ require('../../assets/images/price-icon-defalt-png.png') } alt="" />
@@ -94,22 +135,22 @@ export default function Director() {
                     <div className="driectorBoxItem">
                         <img src={ require('../../assets/images/lock-icon-defalt-png.png') } alt="" />
                         <span className="driectorBoxItemSpan">{t("director.text05")}：</span>
-                        <span>100.000</span>
+                        <span>0.000</span>
                     </div>
                     <div className="driectorBoxItem">
                         <img src={ require('../../assets/images/mine-icon-defalt-png.png') } alt="" />
                         <span className="driectorBoxItemSpan">{t("director.text06")}：</span>
-                        <span>100.000</span>
+                        <span>0.000</span>
                     </div>
                     <div className="driectorBoxItem">
                         <img src={ require('../../assets/images/untie-icon-defalt-png.png') } alt="" />
                         <span className="driectorBoxItemSpan">{t("director.text07")}：</span>
-                        <span>100.000</span>
+                        <span>0.000</span>
                     </div>
                     <div className="driectorBoxItem">
                         <img src={ require('../../assets/images/wallet-icon-defalt-png.png') } alt="" />
                         <span className="driectorBoxItemSpan">{t("director.text08")}：</span>
-                        <span>100.000</span>
+                        <span>{balance}</span>
                     </div>
                 </div>
                 <div className="drictorBut">
