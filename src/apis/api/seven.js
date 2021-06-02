00f1b6dbdd4f79a261f6f153c84fdc1a272dcd26
Qiu-zm <1057390813@qq.com>
2021-06-02 18:55:26 +0800

    Add ttq-husd and ttq-ht

--- src/apis/api/seven.js
+++ src/apis/api/seven.js
@@ -5,12 +5,12 @@ import { htAbi } from '../../abi/ht.abi.js' // next-abi
 import Contract from '../contract/index.js'
 import API from '../contract/api.js'
 
-// pre-next TRS/HT
-const trsAddr = `0x996c26E85C40dC5A2eF4aAF96E286Af1Cf7C58E1` // pre
+// pre-next TTQ/HT
+const trsAddr = `0x7CC64Bee94bA4e40cb609e64B5C72c12d7f2A207` // pre
 const usdtAddr = `0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F` // next
 
-const trsUsdtPairAddr = `0x5c2e8aca453904c6339cf33af6513da944d5241b` // pre-next-pair
-const trsUsdtPoolAddr = `0xa6785e5255321295A90EE3a31A74c7cA1Ef99C80` // pre-next-pool
+const trsUsdtPairAddr = `0x5569bbf9c0e17b6da744617b570e759aa394ad75` // pre-next-pair
+const trsUsdtPoolAddr = `0xaE4BE752568a9F1A54C775995c4d3074136910E6` // pre-next-pool
 
 
 const usdtTokenAddr = usdtAddr
@@ -18,7 +18,7 @@ const huiwanTokenAddr = trsAddr
 const huiwanUsdtMdexAddr = trsUsdtPairAddr
 const huiwanUsdtLoopAddr = trsUsdtPoolAddr
 
-// pre-next TRS/HT
+// pre-next TTQ/HT
 const huiwanTokenABI = trsAbi //pre-abi
 const usdtTokenABI = htAbi //next-abi
 const huiwanUsdtMdexABI = pairAbi
--- src/apis/api/trs_husd.js
+++ src/apis/api/trs_husd.js
@@ -5,12 +5,12 @@ import { husdAbi } from '../../abi/husd.abi.js' // next-abi
 import Contract from '../contract/index.js'
 import API from '../contract/api.js'
 
-// pre-next TRS/HUSD
-const trsAddr = `0x996c26E85C40dC5A2eF4aAF96E286Af1Cf7C58E1` // pre
+// pre-next TTQ/HUSD
+const trsAddr = `0x7CC64Bee94bA4e40cb609e64B5C72c12d7f2A207` // pre
 const usdtAddr = `0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047` // next
 
 const trsUsdtPairAddr = `0xc427816942bf1b92969b3213dd07c26923ee3ee1` // pre-next-pair
-const trsUsdtPoolAddr = `0xF3cB5DC94575bD8ffb517879180c889Fd65737b5` // pre-next-pool
+const trsUsdtPoolAddr = `0x0afC9482D415518045DA02202Ed507c2ded87B40` // pre-next-pool
 
 
 const usdtTokenAddr = usdtAddr
@@ -18,7 +18,7 @@ const huiwanTokenAddr = trsAddr
 const huiwanUsdtMdexAddr = trsUsdtPairAddr
 const huiwanUsdtLoopAddr = trsUsdtPoolAddr
 
-// pre-next TRS/HUSD
+// pre-next TTQ/HUSD
 const huiwanTokenABI = trsAbi //pre-abi
 const usdtTokenABI = husdAbi //next-abi
 const huiwanUsdtMdexABI = pairAbi
